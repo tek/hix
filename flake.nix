8a1abe5d9eeb7d77a2484842895690efe18ebfea
Torsten Schmits <torstenschmits@gmail.com>
2021-07-02 17:41:16 +0200

    bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Haskell Development Build Tools";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/842f900e73c7ce985218cc4f455e34d1d56475c1;
+    nixpkgs.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
     nixpkgs901.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
     nixpkgs8104.url = github:NixOS/nixpkgs/842f900e73c7ce985218cc4f455e34d1d56475c1;
     nixpkgs8105.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
