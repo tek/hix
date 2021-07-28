48ca754d5a6025103edde144d948dee7ce7c63c7
Torsten Schmits <torstenschmits@gmail.com>
2021-07-28 18:50:33 +0200

    remove nixpkgs8105

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
     nixpkgs901.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
     nixpkgs8104.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
-    nixpkgs8105.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
     nixpkgs884.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
     nixpkgs865.url = github:NixOS/nixpkgs/cfed29bfcb28259376713005d176a6f82951014a;
     easy-hls.url = github:jkachmar/easy-hls-nix;
