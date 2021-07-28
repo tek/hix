007ae7b5ed9805e0e8bb01bada1b4f93de428781
Torsten Schmits <torstenschmits@gmail.com>
2021-07-28 18:42:24 +0200

    bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Haskell Development Build Tools";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
-    nixpkgs901.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
-    nixpkgs8104.url = github:NixOS/nixpkgs/842f900e73c7ce985218cc4f455e34d1d56475c1;
-    nixpkgs8105.url = github:NixOS/nixpkgs/432fc2d9a67f92e05438dff5fdc2b39d33f77997;
+    nixpkgs.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
+    nixpkgs901.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
+    nixpkgs8104.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
+    nixpkgs8105.url = github:NixOS/nixpkgs/8ecc61c91a596df7d3293603a9c2384190c1b89a;
     nixpkgs884.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
     nixpkgs865.url = github:NixOS/nixpkgs/cfed29bfcb28259376713005d176a6f82951014a;
     easy-hls.url = github:jkachmar/easy-hls-nix;
