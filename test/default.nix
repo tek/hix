a2349a00e6a0dc47a83daa68613dadc64ce4d35e
Torsten Schmits <git@tryp.io>
2022-01-19 23:32:19 +0100

    move test to a subdirectory

--- test/default.nix
+++ test/default.nix
@@ -1,53 +1,27 @@
 { pkgs, keep ? false, ... }:
-{
+let
+  basic = import ./basic/test.nix { inherit pkgs; };
+in {
   main = pkgs.writeScript "hix-tests" ''
   set -e
   hix_dir=$PWD
   tmpdir=/tmp/hix-test-temp
-  success='true'
+  fail()
+  {
+    echo -e ">>> Test failed!"
+    echo -e ">>> $*"
+    exit 1
+  }
+  ${if keep then "" else ''trap "rm -rf $tmpdir" EXIT''}
 
   rm -rf $tmpdir
   mkdir -p $tmpdir
 
-  cp -r $hix_dir/test/{root,dep1,dep2} $tmpdir/
-  cd $tmpdir
-  sed -i "s#HIX#$hix_dir#" */flake.nix
-  sed -i "s#BASE#$tmpdir#" */flake.nix
-  cd ./root
-  nix flake update
-  nix run .#hpack-verbose
+  testdir="$tmpdir/basic"
+  cp -r $hix_dir/test/basic $testdir
+  cd $testdir
+  ${basic.test}
 
-  # build main package
-  nix build
-  output=$(result/bin/run)
-  if [[ $output != 'success66' ]]
-  then
-    echo ">>> Running the main package failed. Output:"
-    echo $output
-    success='false'
-  fi
-
-  # build minimal derivation of main package
-  nix build .#root.min
-  nix build .#min
-  output=$(result/bin/run)
-  if [[ $output != 'success66' ]]
-  then
-    echo ">>> Running the minimized derivation of the main package failed. Output:"
-    echo $output
-    success='false'
-  fi
-
-  version=$(nix eval .#stm-chans-version.${pkgs.system})
-  if [[ $version != '"2.0.0"' ]]
-  then
-    echo ">>> Test failed: stm-chans version override in 'root' doesn't supersede the one from 'dep1' (is $version)."
-  fi
-  ${if keep then "" else "rm -rf $tmpdir"}
-  if [[ $success == 'false' ]]
-  then
-    exit 1
-  fi
   echo '>>> All tests succeeded.'
   '';
 }
