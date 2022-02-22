50d9598d39aad2b6c28eff81489e6458fccb8cc8
Torsten Schmits <git@tryp.io>
2022-02-23 00:23:35 +0100

    bump nixpkgs for 921

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
-    nixpkgs_ghc921.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
+    nixpkgs_ghc921.url = github:nixos/nixpkgs/7f9b6e2babf232412682c09e57ed666d8f84ac2d;
     nixpkgs_ghc902.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
     nixpkgs_ghc901.url = github:NixOS/nixpkgs/45a3f9d7725c7e21b252c223676cc56fb2ed5d6d;
     nixpkgs_ghc8107.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
