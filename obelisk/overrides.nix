bacc61d4fa7c70fae4710cd538363e1365c4f312
Torsten Schmits <git@tryp.io>
2022-04-11 19:19:20 +0200

    bump obelisk deps

--- obelisk/overrides.nix
+++ obelisk/overrides.nix
@@ -2,10 +2,10 @@
 { hackage, source, configure, override, pkgs, minimal, ... }:
 {
   # reflex
-  universe = hackage "1.2.1" "00876daw7xf1w33szp81r0zp9y6xy6j5pa9i6pvrsf1xybihd6s2";
-  universe-base = hackage "1.1.2" "1k2fizxs7skv36bw7m3d70aj7jp45cprh2s4z4y3sc04if2xcjxc";
+  universe = hackage "1.2.2" "1kdl05l8xfl3439zzmskzka9dffhip8j07zqcdhq28rbb194rra5";
+  universe-base = hackage "1.1.3" "1qj1cljwwplkp3x12pljww3jaswijwhxfpmbdbif38ifc9bzvfgx";
   universe-dependent-sum = hackage "1.3" "17sjgc9g2zxg5bgcqkr9i7cwcg9a8w4w6f2sssww9sr97k5f71js";
-  universe-instances-extended = hackage "1.1.2" "1ilbr6xm5lrvyvg6zsg2manw1h505pjzv5vmymmdziqfys043jc9";
+  universe-instances-extended = hackage "1.1.3" "1lb9mhsdynsinwis4738mx77qc11lrjb9kv3gafvp6f69ch7av62";
   universe-reverse-instances = hackage "1.1.1" "1nd12c7q656bwb51mwsdb58qs9gs25ap2vlblfp102bfbanjlwfn";
   universe-instances-base = hackage "1.1" "1d5hrbn6ydyhkf9i9497rpixrcydryx6il65q4jwjqvrqpnrwgv5";
 
