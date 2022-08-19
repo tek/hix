4bc5d3e85cdf8fa24fff987d0e1c5012a61e4319
Torsten Schmits <git@tryp.io>
2022-08-19 23:55:39 +0200

    use old nixpkgs for nmd because of error

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     nixpkgs_ghc8107.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
     nixpkgs_ghc884.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
     nixpkgs_ghc865.url = github:NixOS/nixpkgs/cfed29bfcb28259376713005d176a6f82951014a;
+    nixpkgs_nmd.url = github:NixOS/nixpkgs/bc41b01dd7a9fdffd32d9b03806798797532a5fe;
     easy-hls.url = github:jkachmar/easy-hls-nix;
     flake-utils.url = github:numtide/flake-utils;
     obelisk = { url = github:tek/obelisk/ghc9; flake = false; };
--- lib/local.nix
+++ lib/local.nix
@@ -1,10 +1,11 @@
 { system, inputs }:
 let
   pkgs = import inputs.nixpkgs { inherit system; };
+  nmd_pkgs = import inputs.nixpkgs_nmd { inherit system; };
 
   tests = system: import ../test/default.nix { inherit pkgs; };
 
-  docs = import ./doc/default.nix { inherit pkgs inputs; };
+  docs = import ./doc/default.nix { inherit inputs; pkgs = nmd_pkgs; };
 
 in {
   packages = {
