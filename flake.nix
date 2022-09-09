2ecc4b75ebf1f44e2e867ff4a4a84d7f03f2cd46
Torsten Schmits <git@tryp.io>
2022-09-09 23:24:52 +0200

    bump all nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,12 +4,10 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
     nixpkgs_ghc942.url = github:nixos/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
-    nixpkgs_ghc924.url = github:nixos/nixpkgs/b42e50fe36242b1b205a7d501b7911d698218086;
-    nixpkgs_ghc902.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
-    nixpkgs_ghc901.url = github:NixOS/nixpkgs/45a3f9d7725c7e21b252c223676cc56fb2ed5d6d;
-    nixpkgs_ghc8107.url = github:NixOS/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7;
+    nixpkgs_ghc924.url = github:nixos/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
+    nixpkgs_ghc902.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
+    nixpkgs_ghc8107.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
     nixpkgs_ghc884.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
-    nixpkgs_ghc865.url = github:NixOS/nixpkgs/cfed29bfcb28259376713005d176a6f82951014a;
     nixpkgs_nmd.url = github:NixOS/nixpkgs/bc41b01dd7a9fdffd32d9b03806798797532a5fe;
     easy-hls.url = github:jkachmar/easy-hls-nix;
     flake-utils.url = github:numtide/flake-utils;
