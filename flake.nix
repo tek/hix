0d90c1df3e90dfacfb600dcbadcf176f7b78f60e
Torsten Schmits <git@tryp.io>
2022-10-01 14:14:45 +0200

    bump nixpkgs for ghc942

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
-    nixpkgs_ghc942.url = github:nixos/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
-    nixpkgs_ghc924.url = github:nixos/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
-    nixpkgs_ghc902.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
-    nixpkgs_ghc8107.url = github:NixOS/nixpkgs/c3e4dc4f6d23c7e41dda5ce98956c75c7779e2fa;
+    nixpkgs_ghc942.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs_ghc924.follows = "nixpkgs";
+    nixpkgs_ghc902.follows = "nixpkgs";
+    nixpkgs_ghc8107.follows = "nixpkgs";
     nixpkgs_ghc884.url = github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e;
     nixpkgs_nmd.url = github:NixOS/nixpkgs/bc41b01dd7a9fdffd32d9b03806798797532a5fe;
     flake-utils.url = github:numtide/flake-utils;
