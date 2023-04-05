d6e5e224c52367a9a8e33af955229993d462d31e
Torsten Schmits <git@tryp.io>
2023-04-05 19:42:15 +0200

    release 0.2.1.0

--- examples/doc-packages/flake.nix
+++ examples/doc-packages/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Example";
-  inputs.hix.url = "github:tek/hix?ref=release/0.2.0.0";
+  inputs.hix.url = "github:tek/hix?ref=release/0.2.1.0";
   outputs = {hix, ...}: hix.lib.flake {
     packages.parser = {
       src = ./.;
