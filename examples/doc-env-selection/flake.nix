7126aec5cdae71aacc4a4219f75e76bd7152e313
Torsten Schmits <git@tryp.io>
2023-04-10 17:45:51 +0200

    Release 0.4.0.2

--- examples/doc-env-selection/flake.nix
+++ examples/doc-env-selection/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "hix test project";
-  inputs.hix.url = "github:tek/hix?ref=0.4.0.1";
+  inputs.hix.url = "github:tek/hix?ref=0.4.0.2";
   outputs = {hix, ...}: hix.lib.auto ({config, ...}: {
     envs = {
       one.env = { number = 1; };
