524e7e22551efa8f8447ef07e73d59e7aad6716b
Torsten Schmits <git@tryp.io>
2023-05-11 15:51:38 +0200

    Release 0.5.3

--- modules/cli.nix
+++ modules/cli.nix
@@ -56,7 +56,7 @@ in {
     } // (
           if config.internal.hixCli.dev
           then { hix = minimal (source.package ../. "hix"); }
-          else { hix = minimal (hackage "0.5.2" "0ff2xyxyldypqr73lvsbj28ia59jr219rbfrk8i4j4kqp3b8rz55"); }
+          else { hix = minimal (hackage "0.5.3" "0qah4pvaqj9ndpj5l1v2slijcfkz6xh6fh0anjlg8yd4mjpd7gp8"); }
       );
 
     ghc = {
