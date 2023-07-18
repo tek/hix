0a3c9b75e0d3a6aa842170e8d0df2969370b719b
Torsten Schmits <git@tryp.io>
2023-07-18 20:51:03 +0200

    remove unnecessary override

--- lib/local.nix
+++ lib/local.nix
@@ -18,7 +18,6 @@ hix.pro ({config, lib, ...}: {
   overrides = {hackage, ...}: {
     exon = hackage "1.4.0.0" "1m4i3a14wip985ncblfy2ikcy7gw5rryj9z497ah218d1nmwj7rl";
     flatparse = hackage "0.4.0.2" "0saxwgwbzijgm9v5w9nx3npl28szpkyz97m4shn8yanxq7gsjnvg";
-    incipit-base = hackage "0.5.0.0" "02fdppamn00m94xqi4zhm6sl1ndg6lhn24m74w24pq84h44mynl6";
   };
 
   cabal = {
