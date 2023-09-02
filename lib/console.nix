87a824e11769fc8f446703f41c4cbe086dc928ed
Torsten Schmits <git@tryp.io>
2023-09-02 17:43:24 +0200

    add some color helpers

--- lib/console.nix
+++ lib/console.nix
@@ -2,20 +2,40 @@
 
   indentLine = l: "  " + l;
 
-  colors = {
-    green = "32";
-    yellow = "33";
-    blue = "34";
-    magenta = "35";
+  colorOffsets = {
+    black = 0;
+    red = 1;
+    green = 2;
+    yellow = 3;
+    blue = 4;
+    magenta = 5;
+    cyan = 6;
+    white = 7;
   };
+
+  colorsBased = base: builtins.mapAttrs (_: o: base + o) colorOffsets;
+
+  colors = colorsBased 30;
+
+  colorsBg = colorsBased 40;
+
+  colorsBright = colorsBased 90;
+
+  colorsBgBright = colorsBased 100;
 
   color = n: t: "\\e[" + toString n + "m" + t + "\\e[0m";
 
 in {
-  inherit indentLine color colors;
+  inherit indentLine color colors colorsBg colorsBright colorsBgBright;
 
   indent = map indentLine;
 
   bold = color "1";
 
+  s.colors = builtins.mapAttrs (_: color) colors;
+  s.colorsBg = builtins.mapAttrs (_: color) colorsBg;
+
+  rgb = r: g: b: color "48;2;${r};${g};${b}";
+
+  colorExt = n: color "48;5;${n}";
 }
