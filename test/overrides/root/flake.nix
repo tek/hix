6555df68603b6ae9c34c3ee575abe453a3fb22b7
Torsten Schmits <git@tryp.io>
2023-09-06 13:38:25 +0200

    fix tests

--- test/overrides/root/flake.nix
+++ test/overrides/root/flake.nix
@@ -18,8 +18,8 @@
     };
     compat.enable = false;
     gen-overrides.enable = true;
-    overrides = {self, pkgs, hackage, jailbreak, buildInputs, ...}: {
-      aeson = jailbreak (hackage "2.1.2.1" "1f1f6h2r60ghz4p1ddi6wnq6z3i07j60sgm77hx2rvmncz4vizp0");
+    overrides = {self, pkgs, hackage, jailbreak, buildInputs, minimal, ...}: {
+      aeson = minimal (jailbreak (hackage "2.1.2.1" "1f1f6h2r60ghz4p1ddi6wnq6z3i07j60sgm77hx2rvmncz4vizp0"));
       extra = jailbreak;
       root1 = jailbreak self.root;
       root = buildInputs [pkgs.git];
