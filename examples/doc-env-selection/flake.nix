0c8907c1dda7f5105ff48430e099926ce7a1fcdb
Torsten Schmits <git@tryp.io>
2023-09-06 13:54:19 +0200

    Release 0.6.3

--- examples/doc-env-selection/flake.nix
+++ examples/doc-env-selection/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "hix test project";
-  inputs.hix.url = "github:tek/hix?ref=0.6.2";
+  inputs.hix.url = "github:tek/hix?ref=0.6.3";
   outputs = {hix, ...}: hix ({config, ...}: {
     envs = {
       one.env = { number = 1; };
--- examples/doc-packages/flake.nix
+++ examples/doc-packages/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Example";
-  inputs.hix.url = "github:tek/hix?ref=0.6.2";
+  inputs.hix.url = "github:tek/hix?ref=0.6.3";
   outputs = {hix, ...}: hix {
     packages.parser = {
       src = ./.;
