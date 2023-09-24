e1c78018792d0a68947451fd3ef0c9c4809479a7
Torsten Schmits <git@tryp.io>
2023-09-24 15:02:54 +0200

    test bootstrap via flake app, not cli

---
+++ test/bootstrap/test-config.nix
@@ -0,0 +1,3 @@
+{...}: {
+  config.internal.hixUrl = "HIX";
+}
--- test/bootstrap/test.nix
+++ test/bootstrap/test.nix
@@ -2,7 +2,7 @@
 {
   test = builtins.toFile "bootstrap-test" ''
     cd ./root
-    nix run path:$hix_dir#cli -- bootstrap --hix-url="path:$hix_dir"
+    check_match 'nix run path:$hix_dir#bootstrap' 'Initialized' "Bootstrap command didn't initialize git repo"
     check_match 'nix run .#ghci -- -p red-panda -t main <<< :quit' 'success' 'Running tests in generated project failed'
   '';
 }
