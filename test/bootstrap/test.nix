2b9cbf6392ee46510b9259033b4e955bad88429d
Torsten Schmits <git@tryp.io>
2023-09-24 16:03:11 +0200

    use nix from pkgs in bootstrap test

--- test/bootstrap/test.nix
+++ test/bootstrap/test.nix
@@ -1,9 +1,9 @@
-{...}:
+{pkgs}:
 {
-  test = builtins.toFile "bootstrap-test" ''
+  test = pkgs.writeText "bootstrap-test" ''
     cd ./root
     git init
-    check_match 'nix run path:$hix_dir#bootstrap' 'generated red-panda-core' "Bootstrap command didn't generate cabal files"
-    check_match 'nix run .#ghci -- -p red-panda -t main <<< :quit' 'success' 'Running tests in generated project failed'
+    check_match '${pkgs.nix}/bin/nix run path:$hix_dir#bootstrap' 'generated red-panda-core' "Bootstrap command didn't generate cabal files"
+    check_match '${pkgs.nix}/bin/nix run .#ghci -- -p red-panda -t main <<< :quit' 'success' 'Running tests in generated project failed'
   '';
 }
