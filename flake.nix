7136b3702afbc8ad9e4961ac65fded5c32e54e2c
Torsten Schmits <git@tryp.io>
2023-11-18 19:37:10 +0100

    bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Haskell/Nix development build tools";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/e12483116b3b51a185a33a272bf351e357ba9a99";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/4f2bd72692b1aae0a6db485ed3a83d1a933c2cac";
 
   outputs = inputs: import ./default.nix inputs;
 }
--- test/basic/test.nix
+++ test/basic/test.nix
@@ -11,7 +11,7 @@
     target='[ "default" "ghc90-root" "ghc92-root" "ghc94-root" "ghc96-root" "min" "root" "static" ]'
     check 'nix eval .#packages.x86_64-linux --apply builtins.attrNames' $target 'Packages are wrong'
 
-    check 'nix develop .#ghc94 -c ghc --version' 'The Glorious Glasgow Haskell Compilation System, version 9.4.6' 'Version output wrong'
+    check 'nix develop .#ghc94 -c ghc --version' 'The Glorious Glasgow Haskell Compilation System, version 9.4.7' 'Version output wrong'
 
     nix build .#ghc92-root
     check 'result/bin/root' 'string' 'Running the main package produced the wrong output'
