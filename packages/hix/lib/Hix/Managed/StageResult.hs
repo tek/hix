240cfe4eb71aae626f4b2e6aadd96c22b711ed55
Torsten Schmits <git@tryp.io>
2024-01-04 21:09:26 +0100

    don't claim to have found new dependencies if no action was required

--- packages/hix/lib/Hix/Managed/StageResult.hs
+++ packages/hix/lib/Hix/Managed/StageResult.hs
@@ -13,7 +13,7 @@ import qualified Hix.Managed.Data.StageResult
 import Hix.Managed.Data.StageResult (
   StageFailure (FailedMutations),
   StageResult (StageResult),
-  StageSummary (StageFailure, StageSuccess),
+  StageSummary (StageFailure, StageNoAction, StageSuccess),
   )
 import qualified Hix.Managed.Data.StageState
 import Hix.Managed.Data.StageState (BuildSuccess, StageState (StageState))
@@ -31,7 +31,8 @@ stageResultWith finalize successMessage failureMessage StageState {success, fail
     summary =
       case nonEmpty (sortOn (.package) failed) of
         Just failedNe -> StageFailure (FailedMutations (failureMessage iterations) (failedMutation <$> failedNe))
-        Nothing -> StageSuccess (successMessage success iterations)
+        Nothing | [] <- success -> StageNoAction (Just "All dependencies are up to date.")
+                | otherwise -> StageSuccess (successMessage success iterations)
 
 stageResult ::
   Pretty a =>
