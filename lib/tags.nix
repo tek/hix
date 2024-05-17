7b598e78244ae0c218fa9a921c4bfe086ebee7b3
Torsten Schmits <git@tryp.io>
2024-05-17 19:48:01 +0200

    bump thax

--- lib/tags.nix
+++ lib/tags.nix
@@ -4,8 +4,8 @@ let
   inherit (config.envs.dev.ghc) compiler pkgs ghc;
 
   thax = import (builtins.fetchTarball {
-    url = "https://github.com/tek/thax/archive/10fa7482d171739414fc0d829005b4055799e4c6.tar.gz";
-    sha256 = "1s1pyblys4mba8cws2fbzmby2k6paxdxls2gcyrhh8dcgbymqmwb";
+    url = "https://github.com/tek/thax/archive/806e9d5a03a168717e0488fad924942774ace90f.tar.gz";
+    sha256 = "1xqk5q477asldpc70br8clk2h3750kby8rc0ddakz27xbr9lkif6";
   }) { inherit pkgs compiler; };
 
   withPrefix =
