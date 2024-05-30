8b617cf66a00d2a7a0423ffb22434ad5b412d655
Torsten Schmits <git@tryp.io>
2024-05-30 04:13:13 +0200

    comments

--- packages/hix/lib/Hix/Managed/Data/Query.hs
+++ packages/hix/lib/Hix/Managed/Data/Query.hs
@@ -5,11 +5,15 @@ import GHC.Exts (IsList)
 import Hix.Data.PackageName (PackageName)
 import Hix.Managed.Data.QueryDep (QueryDep)
 
+-- | User-selected restriction of packages that should be processed.
+-- Given as positional arguments on the command line.
+-- If empty, all packages are processed.
 newtype RawQuery =
   RawQuery [PackageName]
   deriving stock (Eq, Show, Generic)
   deriving newtype (IsList)
 
+-- | Stage-specific subset of the validated environment subset of 'RawQuery', adorned with metadata.
 newtype Query =
   Query (NonEmpty QueryDep)
   deriving stock (Eq, Show, Generic)
