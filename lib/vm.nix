9fd2533ae4835d68c40d17ae420f754c7e6e629f
Torsten Schmits <git@tryp.io>
2024-08-23 20:34:17 +0200

    use loadConsole in vm.nix

--- lib/vm.nix
+++ lib/vm.nix
@@ -5,12 +5,12 @@ let
   if vm.headless then "-display none" else "";
 
   ensure = basePort: vm: util.zscript "ensure-vm" ''
-    #!${pkgs.zsh}/bin/zsh
+    ${util.loadConsole}
     if ${pkgs.procps}/bin/pgrep -F ${vm.pidfile} -L -f ${vm.pidfile} &>/dev/null
     then
-      print '>>> VM already running' >&2
+      message 'VM already running' >&2
     else
-      print '>>> Starting VM with base port ${toString basePort}' >&2
+      message 'Starting VM with base port ${toString basePort}' >&2
       mkdir -p ${vm.dir}
       rm -f ${vm.pidfile}
       ${vm.derivation}/bin/run-nixos-vm ${display vm} -daemonize -pidfile ${vm.pidfile} -monitor unix:${vm.monitor},server,nowait
@@ -18,13 +18,14 @@ let
   '';
 
   kill = vm: util.zscriptErr "kill-vm" ''
+    ${util.loadConsole}
     pid=$(${pkgs.procps}/bin/pgrep -F ${vm.pidfile} -L -f ${vm.pidfile})
     if [[ $? == 0 ]]
     then
-      print '>>> Shutting down VM' >&2
+      message 'Shutting down VM' >&2
       ${pkgs.socat}/bin/socat - UNIX-CONNECT:${vm.monitor} <<< system_powerdown &>/dev/null
     else
-      print '>>> VM not running' >&2
+      error_message 'VM not running' >&2
     fi
   '';
 
