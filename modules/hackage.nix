d4f593f1117f8ada1fb907a5d2a2216fddbf231c
Torsten Schmits <git@tryp.io>
2024-09-07 20:14:48 +0200

    remove open import

--- modules/hackage.nix
+++ modules/hackage.nix
@@ -1,36 +1,35 @@
-{ util, ... }:
-with builtins;
-with util.lib;
+{ config, lib, util, ... }:
 let
+  inherit (lib) types;
 
   logic = import ../lib/hackage.nix { inherit util; };
 
-  isNix = file: match ".*\.nix" file != null;
+  isNix = file: builtins.match ".*\.nix" file != null;
 
 in {
-  options.hackage = with types; {
+  options.hackage = {
 
-    packages = mkOption {
+    packages = lib.mkOption {
       description = ''
         The set of packages that will be published to Hackage when the release command is run without arguments.
         If it is `null`, all packages are published.
         The items in the list should be Cabal package names as defined in `options.packages`.
       '';
-      type = nullOr (listOf str);
+      type = types.nullOr (types.listOf types.str);
       default = null;
     };
 
-    allPackages = mkOption {
+    allPackages = lib.mkOption {
       description = ''
         There are two modes for versioning: Either all packages share the same version, in which case the release app
         will publish all packages at the same time, or each package has an individual version, in which case the release
         app expects the name of a package to be specified.
       '';
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
-    versionFile = mkOption {
+    versionFile = lib.mkOption {
       description = ''
         If multiple packages use the same file for the version (like when using shared hpack files) this option may
         point to that file.
@@ -43,24 +42,24 @@ in {
         The default version handlers make this assumption; if a different method is used, the options
         `hackage.versionFileExtract` and `hackage.versionFileUpdate` must be adapted.
       '';
-      type = nullOr str;
+      type = types.nullOr types.str;
       default = null;
     };
 
-    versionFileExtract = mkOption {
+    versionFileExtract = lib.mkOption {
       description = ''
       A function that returns a shell script fragment that extracts the current version from a version file.
       The default assumes hpack/cabal format, like `version: 5`, unless the file has the extension
       `.nix`, in which case it is assumed the file only contains a string.
       '';
-      type = functionTo str;
+      type = types.functionTo types.str;
       default = file:
       if isNix file
       then ''sed -n 's/"\(.*\)"/\1/p' ${file}''
       else ''sed -n 's/^version:\s*\(\S\+\)/\1/p' ${file}'';
     };
 
-    versionFileUpdate = mkOption {
+    versionFileUpdate = lib.mkOption {
       description = ''
       A function that returns a shell script fragment that updates the current version in a version file.
       The new version is stored in the environment variable `$new_version` in the surrounding shell
@@ -68,74 +67,74 @@ in {
       The default assumes hpack/cabal format, like `version: 5`, unless the file has the extension
       `.nix`, in which case it is assumed the file only contains a string.
       '';
-      type = functionTo str;
+      type = types.functionTo types.str;
       default = file:
       if isNix file
       then ''sed -i "s/\".*\"/\"$new_version\"/" ${file}''
       else ''sed -i "s/^version:\(\s*\).*/version:\1$new_version/" ${file}'';
     };
 
-    setChangelogVersion = mkOption {
+    setChangelogVersion = lib.mkOption {
       description = "Whether to substitute the word 'Unreleased' with the new version in changelogs.";
-      type = bool;
+      type = types.bool;
       default = false;
     };
 
-    check = mkOption {
+    check = lib.mkOption {
       description = ''
         Whether to run `nix flake check` before the release process.
       '';
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
-    commit = mkOption {
+    commit = lib.mkOption {
       description = ''
         After successfully uploading a new release, the changes to the version file, cabal files and changelog will be
         committed unless this is set to `false`.
       '';
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
-    commitExtraArgs = mkOption {
+    commitExtraArgs = lib.mkOption {
       description = "Extra CLI options for `git commit`.";
-      type = str;
+      type = types.str;
       default = "";
     };
 
-    add = mkOption {
+    add = lib.mkOption {
       description = ''
       When [](#opt-hackage-hackage.add) is set to `false`, this option can be enabled to git-add the files but not
       commit them.
       '';
-      type = bool;
+      type = types.bool;
       default = false;
     };
 
-    tag = mkOption {
+    tag = lib.mkOption {
       description = ''
         After successfully uploading a new release, a tag with the version name will be created unless this is set to
         `false`.
       '';
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
-    tagExtraArgs = mkOption {
+    tagExtraArgs = lib.mkOption {
       description = "Extra CLI options for `git tag`.";
-      type = str;
+      type = types.str;
       default = "";
     };
 
-    formatTag = mkOption {
+    formatTag = lib.mkOption {
       description = "Function that creates a tag name from a version and an optional package name.";
-      type = functionTo str;
+      type = types.functionTo types.str;
       default = { name, version }:
       if name == null then version else "${name}-${version}";
     };
 
-    uploadCommand = mkOption {
+    uploadCommand = lib.mkOption {
       description = ''
         The command used to upload a tarball, specified as a function that takes a set as a parameter with the
         attributes:
@@ -147,49 +146,49 @@ in {
         }
         ```
       '';
-      type = functionTo str;
       default = { publish, doc, path }:
       "cabal upload ${if publish then "--publish " else ""}${if doc then "--documentation " else ""}${path}";
+      type = types.functionTo types.str;
     };
 
-    askVersion = mkOption {
+    askVersion = lib.mkOption {
       description = "Whether to interactively query the user for a new version when releasing.";
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
-    confirm = mkOption {
+    confirm = lib.mkOption {
       description = "Whether to ask for confirmation before uploading.";
-      type = bool;
+      type = types.bool;
       default = true;
     };
 
     hooks = {
 
-      postUploadAll = mkOption {
+      postUploadAll = lib.mkOption {
         description = ''
         Shell script lines (zsh) to run after uploading all packages.
 
         Value is a function that gets the set `{source, publish}`, two booleans that indicate whether the sources (or
         only docs) were uploaded, and whether the artifacts were published (or just candidates).
         '';
-        type = functionTo lines;
+        type = types.functionTo types.lines;
         default = _: "";
       };
 
-      preCommitAll = mkOption {
+      preCommitAll = lib.mkOption {
         description = ''
         Shell script lines (zsh) to run before commiting the version change after publishing all packages.
         '';
-        type = lines;
+        type = types.lines;
         default = "";
       };
 
-      postCommitAll = mkOption {
+      postCommitAll = lib.mkOption {
         description = ''
         Shell script lines (zsh) to run after commiting the version change after publishing all packages.
         '';
-        type = lines;
+        type = types.lines;
         default = "";
       };
 
@@ -197,14 +196,14 @@ in {
 
     output = {
 
-      packages = mkOption {
+      packages = lib.mkOption {
         description = "Internal option";
-        type = unspecified;
+        type = types.unspecified;
       };
 
-      apps = mkOption {
+      apps = lib.mkOption {
         description = "Internal option";
-        type = unspecified;
+        type = types.unspecified;
       };
 
     };
