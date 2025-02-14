f2cfc96b0580de4c7f41b33f5733574a8670bfc1
Torsten Schmits <git@tryp.io>
2025-02-14 23:52:08 +0100

    test `warning`: adapt message to nix version

--- test/warning/test.nix
+++ test/warning/test.nix
@@ -1,8 +1,17 @@
 {
   source = ''
-  error_exact "trace: evaluation warning: The option 'packages.*.subpath' is deprecated in favor of 'project.packages.*.path'. 
+  if [[ $(nix --version) =~ '2\.18' ]]
+  then
+    error_exact "\
+  trace: evaluation warning: The option 'packages.*.subpath' is deprecated in favor of 'project.packages.*.path'. 
   For instructions on how to access 'project', see [https://tryp.io/hix#intermediate-outputs].
   Disable this warning by setting 'ui.warnings.keys.\"deprecated.option.packages.subpath\" = false;'"
+  else
+    error_exact "\
+  evaluation warning: The option 'packages.*.subpath' is deprecated in favor of 'project.packages.*.path'.
+                      For instructions on how to access 'project', see [https://tryp.io/hix#intermediate-outputs].
+                      Disable this warning by setting 'ui.warnings.keys.\"deprecated.option.packages.subpath\" = false;'"
+  fi
   output_exact '"."'
   step_eval package-subpath
   '';
