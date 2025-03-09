77e5fad507251ebbf43ad7d31484435bd6daed7a
Torsten Schmits <git@tryp.io>
2025-03-09 14:22:13 +0100

    add env `hix-build-tools` for configuring hpack and cabal-install

---
+++ modules/build-tools.nix
@@ -0,0 +1,31 @@
+{config, lib, ...}: let
+
+  inherit (lib) types;
+
+  ghc = config.envs.hix-build-tools.ghc.ghc;
+
+in {
+
+  options.build-tools = {
+
+    cabal.package = lib.mkOption {
+      description = "A package containing the Cabal executable at `bin/cabal`.";
+      type = types.package;
+    };
+
+    hpack.package = lib.mkOption {
+      description = "A package containing the HPack executable at `bin/hpack`.";
+      type = types.package;
+    };
+
+  };
+
+  config.build-tools = {
+
+    cabal.package = lib.mkDefault ghc.cabal-install;
+
+    hpack.package = lib.mkDefault ghc.hpack;
+
+  };
+
+}
