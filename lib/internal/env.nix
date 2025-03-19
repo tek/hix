f702598a7c323d0bc01787c146b5f17c5207cfd9
Torsten Schmits <git@tryp.io>
2025-03-19 21:23:11 +0100

    modernize some vm scripts

--- lib/internal/env.nix
+++ lib/internal/env.nix
@@ -5,13 +5,13 @@
   waitScript = env: let
     waitSeconds = toString env.wait;
     port = env.hostPorts.hix-internal-env-wait;
-  in ''
+  in util.hixScript "hix-vm-wait" { path = pkgs: [pkgs.socat]; } ''
   running=0
-  echo ">>> Waiting ${waitSeconds} seconds for VM to boot..." >&2
+  message "Waiting ${waitSeconds} seconds for VM to boot..."
   timeout=$(( $SECONDS + ${waitSeconds} ))
   while (( $SECONDS < $timeout ))
   do
-    pong=$(${config.pkgs.socat}/bin/socat -T 1 - TCP:localhost:${toString port} <<< 'ping' 2>&1)
+    pong=$(socat -T 1 - TCP:localhost:${toString port} <<< 'ping' 2>&1)
     if [[ $pong == 'running' ]]
     then
       running=1
@@ -22,7 +22,7 @@
   done
   if [[ $running == 0 ]]
   then
-    echo ">>> VM wasn't ready after ${waitSeconds} seconds." >&2
+    error_message "VM wasn't ready after ${waitSeconds} seconds."
     exit 1
   fi
   '';
--- lib/vm.nix
+++ lib/vm.nix
@@ -1,34 +1,33 @@
 {pkgs, util}:
-with pkgs.lib;
 let
   display = vm:
   if vm.headless then "-display none" else "";
 
-  ensure = basePort: vm: util.zscript "ensure-vm" ''
-    ${util.loadConsole}
-    if ${pkgs.procps}/bin/pgrep -F ${vm.pidfile} -L -f ${vm.pidfile} &>/dev/null
-    then
-      message 'VM already running' >&2
-    else
-      message 'Starting VM with base port ${toString basePort}' >&2
-      mkdir -p ${vm.dir}
-      rm -f ${vm.pidfile}
-      ${vm.derivation}/bin/run-nixos-vm ${display vm} -daemonize -pidfile ${vm.pidfile} -monitor unix:${vm.monitor},server,nowait
-    fi
+  ensure = basePort: vm: util.hixScript "hix-ensure-vm" { path = pkgs: [pkgs.procps]; } ''
+  if pgrep -F ${vm.pidfile} -L -f ${vm.pidfile} &> /dev/null
+  then
+    message 'VM already running'
+  else
+    message 'Starting VM with base port ${toString basePort}'
+    mkdir -p ${vm.dir}
+    rm -f ${vm.pidfile}
+    ${vm.derivation}/bin/run-nixos-vm ${display vm} \
+      -daemonize -pidfile ${vm.pidfile} \
+      -monitor unix:${vm.monitor},server,nowait
+  fi
   '';
 
-  kill = vm: util.zscriptErr "kill-vm" ''
-    ${util.loadConsole}
-    pid=$(${pkgs.procps}/bin/pgrep -F ${vm.pidfile} -L -f ${vm.pidfile})
-    if [[ $? == 0 ]]
-    then
-      message 'Shutting down VM' >&2
-      ${pkgs.socat}/bin/socat - UNIX-CONNECT:${vm.monitor} <<< system_powerdown &>/dev/null
-    else
-      error_message 'VM not running' >&2
-    fi
+  kill = vm: util.hixScript "hix-kill-vm" { path = pkgs: [pkgs.socat pkgs.procps]; } ''
+  pid=$(pgrep -F ${vm.pidfile} -L -f ${vm.pidfile})
+  if [[ $? == 0 ]]
+  then
+    message 'Shutting down VM'
+    socat - UNIX-CONNECT:${vm.monitor} <<< system_powerdown &> /dev/null
+  else
+    error_message 'VM not running'
+  fi
   '';
 
 in {
-  inherit create ensure kill;
+  inherit ensure kill;
 }
