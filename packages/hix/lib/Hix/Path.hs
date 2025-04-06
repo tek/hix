cc7cec87b4e2ec0d9554d2137e6b2ebd9d2e96ae
DeviousStoat <devious@stoat.com>
2025-04-06 16:45:31 +0200

    add app context

--- packages/hix/lib/Hix/Path.hs
+++ packages/hix/lib/Hix/Path.hs
@@ -1,6 +1,7 @@
 module Hix.Path where
 
 import Control.Monad.Trans.Reader (ask)
+import Exon (exon)
 import Path (Abs, Dir, File, Path, parent, toFilePath)
 import Path.IO (resolveDir, resolveFile)
 import System.FilePattern.Directory (getDirectoryFiles)
@@ -9,6 +10,8 @@ import System.IO.Error (tryIOError)
 import qualified Hix.Data.Monad
 import Hix.Data.Monad (AppResources (AppResources), M (M), liftE)
 import Hix.Data.PathSpec (PathSpec, resolvePathSpec)
+import Hix.Monad (appContext)
+import Hix.Pretty (showHP)
 
 findFlake :: Path Abs Dir -> IO (Maybe (Path Abs Dir))
 findFlake cur =
@@ -26,12 +29,17 @@ rootDir :: Maybe (Path Abs Dir) -> M (Path Abs Dir)
 rootDir =
   maybe inferRoot pure
 
+resolvePathSpecWith :: 
+  (Path Abs Dir -> FilePath -> IO (Path Abs t)) ->
+  PathSpec t ->
+  M (Path Abs t)
+resolvePathSpecWith resolver pathSpec =
+  appContext [exon|resolving path spec #{showHP pathSpec}|] $ do
+    AppResources {cwd} <- M ask
+    liftE $ resolvePathSpec resolver cwd pathSpec
+
 resolvePathSpecDir :: PathSpec Dir -> M (Path Abs Dir)
-resolvePathSpecDir pathSpec = do
-  AppResources {cwd} <- M ask
-  liftE $ resolvePathSpec resolveDir cwd pathSpec
+resolvePathSpecDir = resolvePathSpecWith resolveDir
 
 resolvePathSpecFile :: PathSpec File -> M (Path Abs File)
-resolvePathSpecFile pathSpec = do
-  AppResources {cwd} <- M ask
-  liftE $ resolvePathSpec resolveFile cwd pathSpec
+resolvePathSpecFile = resolvePathSpecWith resolveFile
