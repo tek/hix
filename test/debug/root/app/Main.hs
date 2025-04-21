78a981532ad61da6094e0cd9a143d42df13f217d
Torsten Schmits <git@tryp.io>
2025-04-22 00:37:03 +0200

    debug

--- test/debug/root/app/Main.hs
+++ test/debug/root/app/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import GHC.IO.Encoding (getFileSystemEncoding, getLocaleEncoding, setFileSystemEncoding, setLocaleEncoding, utf8)
+
 main :: IO ()
 main = do
+  print =<< getLocaleEncoding
+  print =<< getFileSystemEncoding
+  setLocaleEncoding utf8
+  setFileSystemEncoding utf8
+  print =<< getLocaleEncoding
+  print =<< getFileSystemEncoding
   putStrLn "📦"
--- test/debug/test.nix
+++ test/debug/test.nix
@@ -1,6 +1,11 @@
 {
 
   source = ''
+  LANG=en_US.UTF-8
+  locale
+  print $LANGUAGE
+  print $LC_ALL
+  print $LOCALE_ARCHIVE
   message "Debug test"
   nix run
   '';
