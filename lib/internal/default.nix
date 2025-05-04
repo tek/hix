a31c4b17758f540d576cf6932e49be49b33a1f78
Torsten Schmits <git@tryp.io>
2025-05-04 15:22:13 +0200

    WIP: introduce a general mechanism for options that may refer to or extend existing configurations

--- lib/internal/default.nix
+++ lib/internal/default.nix
@@ -2,6 +2,7 @@
 
   env = import ./env.nix { inherit util; };
   envs = import ./envs.nix { inherit util; };
+  modules = import ./modules.nix { inherit util; };
   package = import ./package.nix { inherit util; };
   packages = import ./packages.nix { inherit util; };
   project = import ./project.nix { inherit util; };
