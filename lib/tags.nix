cb0d67f10d34d50c607a24fec32f900ab8bf2e3e
Torsten Schmits <git@tryp.io>
2025-10-26 13:22:00 +0100

    Get `hasktags` from the env `hix-build-tools`

--- lib/tags.nix
+++ lib/tags.nix
@@ -7,7 +7,7 @@ let
   thax = import (builtins.fetchTarball {
     url = "https://github.com/tek/thax/archive/806e9d5a03a168717e0488fad924942774ace90f.tar.gz";
     sha256 = "1xqk5q477asldpc70br8clk2h3750kby8rc0ddakz27xbr9lkif6";
-  }) { inherit pkgs; compiler = tag; };
+  }) { inherit pkgs; compiler = tag; hasktagsPackage = config.build-tools.hasktags.package; };
 
   withPrefix =
     name: pkg:
