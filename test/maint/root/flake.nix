dd04fd1ac2a251cac438fe843234539c7a094ff9
Torsten Schmits <git@tryp.io>
2025-12-07 20:12:46 +0100

    Remove unused overrides in test `maint`

--- test/maint/root/flake.nix
+++ test/maint/root/flake.nix
@@ -41,9 +41,7 @@
     compat.enable = false;
 
     overrides = {hackage, ...}: {
-      extra = hackage "1.7.16" "0y27q0zas66qbgvjga0w8dmfjcs0kcn9nwps17iyd60ij3yqivhj";
       semigroups = hackage "0.19.2" "1q8ssd9c3rr1d0vjc6vdnl27r9n0p3xxacag6s0ahsa2njz225ra";
-      path = hackage "0.8.0" "0isldidz2gypw2pz399g6rn77x9mppd1mvj5h6ify4pj4mpla0pb";
     };
 
     packages = {
